ffb1d01046e1b804040c9761bcc9b34373df76a8
Daniela280313 <DANI25095@gmail.com>
2022-11-07 16:27:03 -0500

    registro de usuarios

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -8,8 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Usuario extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
     
-    protected $fillable = ['nombre', 'apellido', 'tipo_doc', 'num_doc', 'correo', 'contrasena','tipo_user','estado'];
+    protected $fillable = ['nombre', 'apellido', 'tipo_doc', 'num_doc', 'correo', 'contrasena','tipo_user'];
 
     public function eventos(){
         return $this -> hasMany(Evento::class);
